4816b545035eee88b3080bb136a9f1778d904a1c
Tanya Bihag <tbihag@gmail.com>
2019-12-14 04:04:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   
   elsif array.size == 2 
     return array.join(" and ")
-  else arraysize >= 3
+  else array.size >= 3
     last = array.pop
 		array.join(", ") + ", and " + last
    end 
