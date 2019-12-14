5dd48c2aa84aecd89535e3d00d9715419f60fe49
Tanya Bihag <tbihag@gmail.com>
2019-12-14 04:03:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   if array.size == 1 
-    return array[0]
+    return array.join
   
   elsif array.size == 2 
     return array.join(" and ")
-  else 
+  else arraysize >= 3
+    
     
 
 end
